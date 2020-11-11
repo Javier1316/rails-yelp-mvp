b3f9614a600ad2501cf9e690142f1f44b3229949
Javier Granda <javogroj@gmail.com>
2020-11-11 13:50:51 -0700

    its working now

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restnt1 = Restaurant.create(name: "michaels pizza", address: "calgary", category: "italian" )
+restnt2 = Restaurant.create(name: "pizza73", address: "calgary", category: "italian" )
+restnt3 = Restaurant.create(name: "rec room", address: "edmonton", category: "belgian" )
+restnt4 = Restaurant.create(name: "china palace", address: "edmonton", category: "chinese" )
+restnt5 = Restaurant.create(name: "daruma", address: "calgary", category: "japanese" )
